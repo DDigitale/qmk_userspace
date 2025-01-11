4aa915533076074115ffa869ab1f42aace31e777
DDigitale <alexdvox@gmail.com>
2025-01-11 22:38:52 +0000

    9

--- keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
@@ -158,7 +158,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 void rgb_matrix_update_pwm_buffers(void);
 #endif
 
-layer_state_t layer_state_set_user(layer_state_t state) {
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (get_highest_layer(layer_state) > 0) {
         uint8_t layer = get_highest_layer(layer_state);
 
@@ -168,7 +168,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
                 if (index >= led_min && index < led_max && index != NO_LED &&
                 keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_GREEN);
+                    rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
+                    rgb_matrix_sethsv_noeeprom(HSV_GREEN);
                 }
             }
         }
