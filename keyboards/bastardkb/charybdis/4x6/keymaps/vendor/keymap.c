a1794f06eefff6a60db0c7169b648a7cbe2a2217
DDigitale <alexdvox@gmail.com>
2025-01-12 00:29:44 +0000

    29

--- keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
@@ -150,25 +150,56 @@ void matrix_scan_user(void) {
 #    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
 #    ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Включить подсветку для POINTER слоя
-    if (layer_state_cmp(state, LAYER_POINTER)) {
-#        ifdef RGB_MATRIX_ENABLE
-        rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-        rgb_matrix_update_pwm_buffers();
-#        endif
-    } else {
-#        ifdef RGB_MATRIX_ENABLE
-        rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
-        rgb_matrix_update_pwm_buffers();
-#        endif
-    }
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//     // Включить подсветку для POINTER слоя
+//     if (layer_state_cmp(state, LAYER_POINTER)) {
+// #        ifdef RGB_MATRIX_ENABLE
+//         rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+// #        endif
+//     } else {
+// #        ifdef RGB_MATRIX_ENABLE
+//         rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
+// #        endif
+//     }
 
-    charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
-    return state;
-}
+//     charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
+//     return state;
+// }
 #    endif // CHARYBDIS_AUTO_SNIPING_ON_LAYER
 #endif     // POINTING_DEVICE_ENABLE
+
+#ifdef RGB_MATRIX_ENABLE
+// Обработчик изменения состояния слоев с подсветкой.
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t active_layer = get_highest_layer(state);
+
+    // Интегрируем вашу логику подсветки для различных слоев
+    switch (active_layer) {
+        case LAYER_LOWER:
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+            rgb_matrix_sethsv_noeeprom(HSV_YELLOW);  // Жёлтый для LOWER слоя
+            break;
+        case LAYER_RAISE:
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+            rgb_matrix_sethsv_noeeprom(HSV_BLUE);    // Синий для RAISE слоя
+            break;
+        case LAYER_POINTER:
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+            rgb_matrix_sethsv_noeeprom(HSV_GREEN);   // Зелёный для POINTER слоя
+            break;
+        default:
+            rgb_matrix_reload_from_eeprom();
+            break;
+    }
+
+    // Ваша оригинальная логика для автоматического включения режима снайпера на слое POINTER
+    #ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
+    charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
+    #endif
+
+    return state;
+}
+#endif // RGB_MATRIX_ENABLE
 
 #ifdef RGB_MATRIX_ENABLE
 // Forward-declare this helper function since it is defined in rgb_matrix.c.
