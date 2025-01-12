fde87820a12af5a8eff1aa68a9882faf890cc633
DDigitale <alexdvox@gmail.com>
2025-01-12 00:40:10 +0000

    30

--- keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
@@ -118,55 +118,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
-#ifdef POINTING_DEVICE_ENABLE
-#    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
-        if (auto_pointer_layer_timer == 0) {
-            layer_on(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-            rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
-            rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-            rgb_matrix_update_pwm_buffers();  // Обновление подсветки
-#        endif // RGB_MATRIX_ENABLE
-        }
-        auto_pointer_layer_timer = timer_read();
-    }
-    return mouse_report;
-}
-
-void matrix_scan_user(void) {
-    if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
-        auto_pointer_layer_timer = 0;
-        layer_off(LAYER_POINTER);
-
-        // Восстановить подсветку
-#        ifdef RGB_MATRIX_ENABLE
-        rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);  // Включить стандартный режим подсветки
-        rgb_matrix_update_pwm_buffers();  // Обновление подсветки
-#        endif
-    }
-}
-#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-
-#    ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//     // Включить подсветку для POINTER слоя
-//     if (layer_state_cmp(state, LAYER_POINTER)) {
+// #ifdef POINTING_DEVICE_ENABLE
+// #    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
+//         if (auto_pointer_layer_timer == 0) {
+//             layer_on(LAYER_POINTER);
 // #        ifdef RGB_MATRIX_ENABLE
-//         rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-// #        endif
-//     } else {
+//             rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
+//             rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+//             rgb_matrix_update_pwm_buffers();  // Обновление подсветки
+// #        endif // RGB_MATRIX_ENABLE
+//         }
+//         auto_pointer_layer_timer = timer_read();
+//     }
+//     return mouse_report;
+// }
+
+// void matrix_scan_user(void) {
+//     if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
+//         auto_pointer_layer_timer = 0;
+//         layer_off(LAYER_POINTER);
+
+//         // Восстановить подсветку
 // #        ifdef RGB_MATRIX_ENABLE
-//         rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
+//         rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);  // Включить стандартный режим подсветки
+//         rgb_matrix_update_pwm_buffers();  // Обновление подсветки
 // #        endif
 //     }
-
-//     charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
-//     return state;
 // }
-#    endif // CHARYBDIS_AUTO_SNIPING_ON_LAYER
-#endif     // POINTING_DEVICE_ENABLE
+// #    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+
+// #    ifdef CHARYBDIS_AUTO_SNIPING_ON_LAYER
+// // layer_state_t layer_state_set_user(layer_state_t state) {
+// //     // Включить подсветку для POINTER слоя
+// //     if (layer_state_cmp(state, LAYER_POINTER)) {
+// // #        ifdef RGB_MATRIX_ENABLE
+// //         rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+// // #        endif
+// //     } else {
+// // #        ifdef RGB_MATRIX_ENABLE
+// //         rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
+// // #        endif
+// //     }
+
+// //     charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
+// //     return state;
+// // }
+// #    endif // CHARYBDIS_AUTO_SNIPING_ON_LAYER
+// #endif     // POINTING_DEVICE_ENABLE
 
 #ifdef RGB_MATRIX_ENABLE
 // Обработчик изменения состояния слоев с подсветкой.
